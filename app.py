ade3b2dc591b4024d282a308bcf110931ec2414a
NikitaCase <69657536+NikitaCase@users.noreply.github.com>
2021-02-02 23:32:14 -0500

    entertainment route
    entertainment route added, links to other python files removed

--- app.py
+++ app.py
@@ -1,80 +1,79 @@
-# import necessary libraries
-from models import create_classes
-import os
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+# Import Libraries
+#----------------------------------------------------------------------------
 
-#################################################
+import os
+from flask import Flask, render_template, jsonify
+ 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+
+#------------------------------------------------------------------------------
+# Create an engine for the database
+#------------------------------------------------------------------------------
+engine = create_engine('postgres://gvrxbsgl:OSgGmghNCc6_K8YCTvPaVp4jmm78ezbm@raja.db.elephantsql.com:5432/gvrxbsgl', echo=False)
+#cxn = engine.connect()
+
+# Reflect Database into ORM classes
+#------------------------------------------------------------------------------
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+print(Base.classes.keys())
+
+
+#------------------------------------------------------------------------------
 # Flask Setup
-#################################################
+#------------------------------------------------------------------------------
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
 
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-Pet = create_classes(db)
-
-# create route that renders index.html template
+# Frontend Route 
+#------------------------------------------------------------------------------
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
+# Backend Routes
+#------------------------------------------------------------------------------
+@app.route('/entertainment')
+def entertainment(): 
+    
+    stocks = Base.classes.combined_table
+    session = Session(engine)
 
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
-
-    return render_template("form.html")
+    gc = session.query(stocks.Ticker, stocks.Adj_Close, stocks.date).filter(stocks.Ticker =='GC.TO').order_by(stocks.date).all()
+    recp = session.query(stocks.Ticker, stocks.Adj_Close, stocks.date).filter(stocks.Ticker =='RECP.TO').order_by(stocks.date).all()
+    cgx = session.query(stocks.Ticker, stocks.Adj_Close, stocks.date).filter(stocks.Ticker =='CGX.TO').order_by(stocks.date).all()
 
 
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+    entertainment_stocks =[
+        {
+        'Ticker': 'GC.TO',
+        'Date': [row[2] for row in gc],
+        'Adj_Close': [row[1] for row in gc], 
+        }, 
+              {
+        'Ticker': 'RECP.TO',
+        'Date': [row[2] for row in recp],
+        'Adj_Close': [row[1] for row in recp], 
+        }, 
+              {
+        'Ticker': 'CGX.TO',
+        'Date': [row[2] for row in cgx],
+        'Adj_Close': [row[1] for row in cgx], 
+        } 
+    ]
+    
+    return jsonify(entertainment_stocks)
 
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
 
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
-
-    return jsonify(pet_data)
-
+#------------------------------------------------------------------------------
+# Debugging
+#------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     app.run()

7e7debaca3b11101b4c74898733a2d9460486e7e
MuratCKOC <muratcagrikoc@gmail.com>
2021-02-04 20:47:02 -0500

    populate js and html

--- stocksvscovid/static/js/app.js
+++ stocksvscovid/static/js/app.js
@@ -1,34 +1,54 @@
-function buildPlot() {
-  /* data route */
-  const url = "/api/pals";
-  d3.json(url).then(function(response) {
 
-    console.log(response);
+function init() {
+  //Html binding
+  var selector = d3.select("#selDateset");
 
-    const data = response;
 
-    const layout = {
-      scope: "usa",
-      title: "Pet Pals",
-      showlegend: false,
-      height: 600,
-            // width: 980,
-      geo: {
-        scope: "usa",
-        projection: {
-          type: "albers usa"
-        },
-        showland: true,
-        landcolor: "rgb(217, 217, 217)",
-        subunitwidth: 1,
-        countrywidth: 1,
-        subunitcolor: "rgb(255,255,255)",
-        countrycolor: "rgb(255,255,255)"
-      }
-    };
+  // const url = `/api/${date}`
+  //d3.json(url).then(function(data) => {
+    // Populate the dropdown
+    d3.json("/dates").then((data) =>  {
+        var Dates = data.date;
+        Dates.forEach((date) => {
+            selector.append("option").text(date).property("value",date);
+        })
+        console.log(Dates);
 
-    Plotly.newPlot("plot", data, layout);
+        // Use the first sample to init the charts
+        const initialSample = Dates[0];
+        build_Charts(initialSample);
+        buildMetadata(initialSample)
+    });
+};
+
+
+function optionChanged(newDate) {
+  // Fetch new data each time a new sample is selected
+  // buildCharts(newDate);
+  // buildMetadata(newDate);
+  }
+
+// Initialize the dashboard
+init();
+
+/* function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDateset");
+
+  const url = "/dates"
+  // Use the list of sample names to populate the select options
+  d3.json(url).then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
   });
-}
+} */
 
-buildPlot();

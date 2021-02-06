f7313fe27508683601e31001d2bf0036302c8c28
MuratCKOC <muratcagrikoc@gmail.com>
2021-02-06 13:23:18 -0500

    populate dates

--- stocksvscovid/static/js/app.js
+++ stocksvscovid/static/js/app.js
@@ -147,58 +147,30 @@ buildplot_aviation()
 
 */
 
-// function init() {
-//   //Html binding
-//   console.log("YOU ARE IN IT");
-//   var selector = d3.select("#selDateset");
+function init() {
 
-//   // const url = `/api/${date}`
-//   //d3.json(url).then(function(data) => {
-//     // Populate the dropdown
-//     d3.json("/dates").then((data) =>  {
+  //Html binding
+  var selector = d3.select("#selDateset");
 
-//         console.log(data);
-//         var Dates = data.Date;
+    // Populate the dropdown
+    d3.json("/dates").then((data) =>  {
 
-//         Dates.forEach((date) => {
-//             selector.append("option")
-//             .text(date)
-//             .property("value",date);
-//         })
-//         console.log(Dates);
-//         console.log("AFTER FOREACH");
-//         // Use the first sample to init the charts
-//         const initialSample = Dates[0];
-//         console.log(Dates[0]);
-//         // build_Charts(initialSample);
-//         // buildMetadata(initialSample);
-//     });
-// };
+        var Dates = data.Story[0].Date
 
-// function optionChanged(newDate) {
-//   // Fetch new data each time a new sample is selected
-//   console.log("OPTION CHANGED");
-//   // buildMetadata(newDate);
-//   }
+        Dates.forEach((date) => {
+            selector.append("option")
+            .text(date)
+            .property("value",date);
+        })
+        
+    });
+};
+
+function optionChanged(newDate) {
+  // Fetch new data each time a new sample is selected
+  console.log("OPTION CHANGED");
+  // buildMetadata(newDate);
+  }
 
 // // Initialize the dashboard
-// init();
-
-/* function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDateset");
-  const url = "/dates"
-  // Use the list of sample names to populate the select options
-  d3.json(url).then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-} */
+ init();

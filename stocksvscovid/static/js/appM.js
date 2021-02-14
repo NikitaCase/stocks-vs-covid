622d0ec2a3759972901c17b5b84c4889225c85b3
MuratCKOC <muratcagrikoc@gmail.com>
2021-02-13 22:52:08 -0500

    load news

--- stocksvscovid/static/js/appM.js
+++ stocksvscovid/static/js/appM.js
@@ -3,25 +3,73 @@
 // Select page sections
 var date_selector = d3.select("#selDateset")
 var plot_area = d3.select("#plot")
-var headline = d3.select("#news_headline")
+var headline = d3.select("#news-headline")
+var news = d3.select("#news-text")
 var bar_area = d3.select("#bar-graph")
 var category = d3.select("#selTable")
 
 // Initialization function for index page
 function init() {
+    // load Dates
+    load_Dates();
+    // load News
+    load_News();
+    // load Categories
+
+
+        // Bring up first news
+        //load_News(date_selector.node().value);
+        //load_News(select.node().value);
+};
+
+
+
+// load Dates Function
+function load_Dates(){
     d3.json("/dates").then((data) => {
         // Pull data from news date table
         var Dates = data.Story[0].Date
-        var News = data.Story[0].News
 
-        //Populate Dates selector section
+        // Populate Dates selector section
         Dates.forEach((date) => {
+            var date = date
             date_selector.append("option")
-                .text(date)
+                .text(date.slice(0, -13))
                 .property("value",date);
-        })
+            console.log("ASD")
+        });
     });
-};
+
+    console.log("DATES: ",date_selector.node().value);
+}
+
+// load News Function
+//missing--->> Should load the first news on the page initialization
+function load_News(){
+    var user_date = date_selector.node().value;
+
+    console.log("NEWS DATE",user_date)
+
+    headline.html("")
+    headline.append("h3")
+        .text(`Headline for ${user_date.slice(0, -13)}`)
+
+    news.html("")
+    d3.json("/dates").then((data) => {
+        console.log(data)
+        var dates = data.Story[0]['Date']
+        var ind = dates.indexOf(user_date)
+        //m---> Setindex 0 to load at initialization
+        var blurb = data.Story[0]['News'][ind]
+        news.append("p").text(blurb)
+    });
+}
+// PLOT FUNCTIONS
+
+
+
+
+
 
 // Initialize the page
 init();
@@ -29,13 +77,13 @@ init();
 // Selectors on Change event listeners
 function optionChanged() {
     console.log('Option Changed');
+    load_Dates();
+    load_News();
+    //call plot functions
 }
 
 // Date selector listener
-date_selector.on("change",optionChanged());
+//date_selector.on("change",optionChanged());
 // Category listener
-//
+//category.on("change",optionChanged());
 // Ticker Listener
-
-
-// PLOT FUNCTIONS

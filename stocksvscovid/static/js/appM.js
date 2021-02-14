1e84d5e0e0447c363dc8dccde34af7bb73476b1f
MuratCKOC <muratcagrikoc@gmail.com>
2021-02-13 20:59:59 -0500

    declare variables

---
+++ stocksvscovid/static/js/appM.js
@@ -0,0 +1,9 @@
+// Create a Line plot with Plotly
+
+// Select page sections
+var date_selector = d3.select("#selDateset")
+var plot_area = d3.select("#plot")
+var headline = d3.select("#news_headline")
+var bar_area = d3.select("#bar-graph")
+var category = d3.select("#selTable")
+

aff3a106986533bb4801d477f833ab1da9b57d4f
MuratCKOC <muratcagrikoc@gmail.com>
2021-02-13 21:10:12 -0500

    initalize page, date selector

--- stocksvscovid/static/js/appM.js
+++ stocksvscovid/static/js/appM.js
@@ -7,3 +7,35 @@ var headline = d3.select("#news_headline")
 var bar_area = d3.select("#bar-graph")
 var category = d3.select("#selTable")
 
+// Initialization function for index page
+function init() {
+    d3.json("/dates").then((data) => {
+        // Pull data from news date table
+        var Dates = data.Story[0].Date
+        var News = data.Story[0].News
+
+        //Populate Dates selector section
+        Dates.forEach((date) => {
+            date_selector.append("option")
+                .text(date)
+                .property("value",date);
+        })
+    });
+};
+
+// Initialize the page
+init();
+
+// Selectors on Change event listeners
+function optionChanged() {
+    console.log('Option Changed');
+}
+
+// Date selector listener
+date_selector.on("change",optionChanged());
+// Category listener
+//
+// Ticker Listener
+
+
+// PLOT FUNCTIONS
